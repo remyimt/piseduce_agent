4a794810e773727ef593e3f03f16ccedb2c5623b
RP <remy.pottier@imt-atlantique.fr>
2021-04-20 16:56:31 +0200

    remove the process that allows users to create images of deployed environments

--- raspberry/action_exec.py
+++ raspberry/action_exec.py
@@ -8,13 +8,24 @@ from paramiko.ssh_exception import BadHostKeyException, AuthenticationException,
 from raspberry.states import SSH_IDX
 import logging, os, paramiko, random, shutil, socket, string, subprocess, time
 
+# Test if the processus exists on the remote node
+def ps_ssh(ssh_session, process):
+    try:
+        (stdin, stdout, stderr) = ssh_session.exec_command("ps aux | grep %s | grep -v grep | wc -l" % process)
+        return_code = stdout.channel.recv_exit_status()
+        output = stdout.readlines()
+        return int(output[0].strip())
+    except SSHException:
+        return -1
+
+
+# Generate a random string of letters and digits
 def new_password(stringLength=8):
-    """Generate a random string of letters and digits """
     lettersAndDigits = string.ascii_letters + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
 
 
-# Deploy environments
+# States of the 'deploy' process (deploy environments)
 def boot_conf_exec(action, db):
     serial = db.query(NodeProperty
             ).filter(NodeProperty.name  == action.node_name
@@ -633,339 +644,12 @@ def user_conf_exec(action, db):
     return False
 
 
-# Test if the processus exists on the remote node
-def ps_ssh(ssh_session, process):
-    try:
-        (stdin, stdout, stderr) = ssh_session.exec_command("ps aux | grep %s | grep -v grep | wc -l" % process)
-        return_code = stdout.channel.recv_exit_status()
-        output = stdout.readlines()
-        return int(output[0].strip())
-    except SSHException:
-        return -1
-
-
-# Create environment images
-def img_part_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    logging.info("[%s] create the image '%s'" % (action.node_name, node.temp_info))
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        cmd = "fdisk -l /dev/mmcblk0"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        output = stdout.readlines()
-        if len(output) > 10:
-            logging.error("[%s] more than 2 partitions detected. Can not create another partition!" %
-            action.node_name)
-            ret_fct = False
-        else:
-            # Check there is enough free space on the disk
-            nb_sectors = int(output[0].split()[-2])
-            last_sector = int(output[-1].split()[2])
-            # Size of the new partition (size occupied by the filesystem * 1.7 to compress the image) in sectors
-            partition_size = int(last_sector * 1.7)
-            # We need partition_size available sectors + 20 sectors
-            if nb_sectors - last_sector > partition_size + 20:
-                # Install the pv tool
-                cmd = "apt install pv"
-                (stdin, stdout, stderr) = ssh.exec_command(cmd)
-                return_code = stdout.channel.recv_exit_status()
-                logging.info("[%s] nb sectors: %d, last sector: %d" % (action.node_name, nb_sectors, last_sector))
-                # Create a new partition that begins at (last_sector + 20 sectors)
-                cmd = ("(echo n; echo ''; echo ''; echo '%d'; echo '+%d'; echo w) | fdisk -u /dev/mmcblk0" %
-                        ((last_sector + 20), partition_size))
-                (stdin, stdout, stderr) = ssh.exec_command(cmd)
-                return_code = stdout.channel.recv_exit_status()
-                # Save the end of the user partitions
-                node.temp_info = "%s %d" % (node.temp_info, last_sector)
-                ret_fct = True
-            else:
-                logging.error("[%s] no enough space on the disk. Only %d sectors available, %d required sectors" %
-                        (action.node_name, (nb_sectors - last_sector), partition_size))
-        ssh.close()
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-
-def img_format_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        cmd = "partprobe"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        # Get the name of the new partition
-        cmd = "ls /dev/mmcblk0p*"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        output = stdout.readlines()
-        partition_name = output[-1].strip()
-        logging.info("[%s] format the partition '%s'" % (action.node_name, partition_name))
-        cmd = "mkfs.ext4 %s &> /dev/null &" % partition_name
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        ssh.close()
-        # Save the partition name
-        node.temp_info = "%s %s" % (node.temp_info, partition_name)
-        return True
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return False
-
-
-def img_copy_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        end_partition = int(node.temp_info.split()[1])
-        partition_name = node.temp_info.split()[2]
-        cmd = "mkdir img_part"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        if ps_ssh(ssh, partition_name) == 0:
-            # Check if the new partition is mounted
-            cmd = "mount -f | grep img_part"
-            (stdin, stdout, stderr) = ssh.exec_command(cmd)
-            return_code = stdout.channel.recv_exit_status()
-            output = stdout.readlines()
-            mounted_part = len(output) > 0
-            if not mounted_part:
-                logging.info("[%s] mount the partition '%s'" % (action.node_name, partition_name))
-                cmd = "mount %s img_part" % partition_name
-                (stdin, stdout, stderr) = ssh.exec_command(cmd)
-                return_code = stdout.channel.recv_exit_status()
-                mounted_part = return_code == 0
-            if mounted_part:
-                # Write the size of the uncompressed image file to the JSON
-                env_name = node.temp_info.split()[0]
-                env_file_path = cluster_desc["env_cfg_dir"] + env_name + ".json"
-                with open(env_file_path, "r") as jsonfile:
-                    env_data = json.load(jsonfile)
-                env_data["img_size"] = (end_partition + 10) * 512
-                with open(env_file_path, "w") as jsonfile:
-                    json.dump(env_data, jsonfile)
-                # Copy the system to the image file
-                cmd = "dd if=/dev/mmcblk0 of=img_part/%s.img bs=512 count=%d &" % (env_name, end_partition + 10)
-                (stdin, stdout, stderr) = ssh.exec_command(cmd)
-                return_code = stdout.channel.recv_exit_status()
-                ret_fct = True
-                node.temp_info = 0
-            else:
-                logging.error("[%s] can not mount the partition '%s'" % (action.node_name, partition_name))
-        ssh.close()
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-def img_copy_check_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        if ps_ssh(ssh, "count") == 0:
-            ret_fct = True
-        else:
-            cmd = f"tail -n 1 progress-{server['name']}.txt"
-            (stdin, stdout, stderr) = ssh.exec_command(cmd)
-            return_code = stdout.channel.recv_exit_status()
-            output = stdout.readlines()
-            if len(output) == 0:
-                logging.warning("[%s] no progress value for the running image copy" % action.node_name)
-                updated = datetime.datetime.strptime(str(node.updated_at), "%Y-%m-%d %H:%M:%S")
-                elapsedTime = (datetime.datetime.now() - updated).total_seconds()
-                # Compute the progress value with an assumed transfert rate of 8 MB/s
-                percent = elapsedTime * 8000000 * 100 / environment["img_size"]
-            else:
-                percent = output[0].strip()
-            node.temp_info = int(percent)
-        ssh.close()
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-
-def img_customize_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        # Get a free loop device
-        cmd = "losetup -f"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        loop_device = stdout.readlines()[0].strip()
-        logging.info("[%s] delete the third partition by using '%s'" % (action.node_name, loop_device))
-        # Link the .img file to the loop device
-        cmd = "cd img_part; losetup -P %s *.img" % loop_device
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        # Delete the third partition
-        cmd = "(echo d; echo 3; echo w) | fdisk -u %s" % loop_device
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        # Release the loop device
-        cmd = "losetup -d %s" % loop_device
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        ret_fct = True
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-
-def img_compress_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        # Compress the filesystem image
-        cmd = "cd img_part; gzip -9 *.img &"
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
-        return_code = stdout.channel.recv_exit_status()
-        ssh.close()
-        ret_fct = True
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-
-def img_compress_check_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    ret_fct = False
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        if ps_ssh(ssh, "gzip") == 0:
-            # Get both the filename and the size of the compressed image file
-            cmd = "rm -f progress-{server['name']}.txt; cd img_part; ls -l *.img.gz | tail -n 1"
-            (stdin, stdout, stderr) = ssh.exec_command(cmd)
-            return_code = stdout.channel.recv_exit_status()
-            output = stdout.readlines()[0].split()
-            node.temp_info = "%s %s" % (output[-1], output[4])
-            ret_fct = True
-        else:
-            cmd = f"tail -n 1 progress-{server['name']}.txt"
-            (stdin, stdout, stderr) = ssh.exec_command(cmd)
-            return_code = stdout.channel.recv_exit_status()
-            output = stdout.readlines()
-            if len(output) == 0:
-                logging.warning("[%s] no progress value for the running image compression" % action.node_name)
-                updated = datetime.datetime.strptime(str(node.updated_at), "%Y-%m-%d %H:%M:%S")
-                elapsedTime = (datetime.datetime.now() - updated).total_seconds()
-                # Compute the progress value from past experiments on Pi 3B+
-                percent = elapsedTime * 1500000 * 100 / environment["img_size"]
-            else:
-                percent = output[0].strip()
-            node.temp_info = percent
-        ssh.close()
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return ret_fct
-
-
-def upload_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    img_name = node.temp_info.split()[0]
-    img_size = node.temp_info.split()[1]
-    logging.info("[%s] upload the file '%s'" % (action.node_name, img_name))
-    cmd = f"""rsh -o "StrictHostKeyChecking no" %s@%s "cat img_part/%s" | \
-                pv -n -p -s %s 2> /tmp/progress-{action.node_name}.txt | dd of=%s%s bs=4M conv=fsync &""" % (
-                        environment["ssh_user"], action.node_ip, img_name, img_size,
-                        cluster_desc["img_dir"], img_name)
-    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    node.temp_info = 0
-    return True
-
-
-def upload_check_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    cmd = "ps aux | grep %s | grep -v grep | wc -l" % action.node_ip
-    process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    nb_line = process.stdout.decode("utf-8").strip()
-    if int(nb_line) == 0:
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-            cmd = f"rm progress-{server['name']}.txt"
-            (stdin, stdout, stderr) = ssh.exec_command(cmd)
-            return_code = stdout.channel.recv_exit_status()
-            ssh.close()
-            # Load the new environement to the cluster description
-            load_cluster_desc()
-            return True
-        except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-            logging.warning("[%s] SSH connection failed" % action.node_name)
-    else:
-        cmd = f"tail -n 1 /tmp/progress-{server['name']}.txt"
-        process = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-        output = process.stdout.decode("utf-8").strip()
-        if len(output) == 0:
-            logging.warning("[%s] no progress value for the running environment copy" % action.node_name)
-            updated = datetime.datetime.strptime(str(node.updated_at), "%Y-%m-%d %H:%M:%S")
-            elapsedTime = (datetime.datetime.now() - updated).total_seconds()
-            # Compute the progress value with an assumed transfert rate of 8 MB/s
-            percent = elapsedTime * 8000000 * 100 / environment["img_size"]
-        else:
-            percent = output
-        node.temp_info = int(percent)
-    return False
-
-
-# Hard Reboot nodes (turn_off -> turn_on -> coming_back)
-def coming_back_exec(action, db):
-    server = cluster_desc["nodes"][action.node_name]
-    environment = cluster_desc["environments"][node.environment]
-    try:
-        # Test the SSH connection
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        if use_env_ssh_user(node.temp_info):
-            ssh.connect(action.node_ip, username=environment["ssh_user"], timeout=1.0)
-        else:
-            ssh.connect(action.node_ip, username="root", timeout=1.0)
-        ssh.close()
-        # Come back to the deployment process
-        node.process = "deploy"
-        node.state = node.temp_info
-        node.temp_info = None
-        # Always return False to avoid rewriting the node.state property
-        return False
-    except (BadHostKeyException, AuthenticationException, SSHException, socket.error) as e:
-        logging.warning("[%s] SSH connection failed" % action.node_name)
-    return False
-
-
 # Destroying deployments
 def destroying_exec(action, db):
     server = cluster_desc["nodes"][action.node_name]
+    environment = cluster_desc["environments"][node.environment]
     # When destroying initialized deployments, the environment is unset
-    if node.environment is not None:
+    if environment is not None:
         environment = cluster_desc["environments"][node.environment]
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -1003,7 +687,8 @@ def destroying_exec(action, db):
                     (stdin, stdout, stderr) = ssh.exec_command("rpi-eeprom-config | grep BOOT_ORDER")
                     return_code = stdout.channel.recv_exit_status()
                     output = stdout.readlines()
-                    if len(output) > 0 and output[0].strip() != "BOOT_ORDER=0x2":
+                    if len(output) > 0 and (
+                        output[0].strip() != "BOOT_ORDER=0x2" or output[0].strip() != "BOOT_ORDER=0x12"):
                         logging.error("[%s] wrong boot order value. Please update the EEPROM config!" % action.node_name)
                         return False
                 else:
